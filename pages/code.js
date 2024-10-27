51e177cc5cd4f85c306a88e959ca4f2a8d30e7be
Manuel Llanillo <manuel.llanillo.oa22@gmail.com>
2024-10-27 04:25:03 -0700

    Co-authored-by: Sean-Nightingale <Sean-Nightingale@users.noreply.github.com>

---
+++ pages/code.js
@@ -0,0 +1,19 @@
+function runCode() {
+    let interpreterCode = document.getElementById('interpreter-id');
+    let terminalOutput = document.getElementById('terminal-id');
+
+    console.log(interpreterCode);
+    console.log(terminalOutput);
+
+    /* 
+    Aiden's code will do something with the given interpreter code and whatever else here
+    */
+
+    var aidensOutput = "";
+
+    terminalOutput.textContent = aidensOutput;
+};
+
+function pasteCode(elementId) {
+    
+};
